8e645268e3e17f5c1b142b21f9ae0bfc1159a390
DanAndersong <mr.den.andersong@gmail.com>
2019-09-29 21:58:15 +0300

    add changes in pam.xml q faf
    f

--- programms/hackathonParser/src/main/java/hackathonParser/App.java
+++ programms/hackathonParser/src/main/java/hackathonParser/App.java
@@ -12,37 +12,37 @@ public class App
 {
     public static void main( String[] args ) throws IOException {
 //        GUI gui = new GUI();
-
-        Document doc = Jsoup.connect("https://it-events.com/hackathons")
-                .userAgent("Chrome/4.0.249.0 Safari/532.5")
-                .referrer("http://www.google.com")
-                .get();
-
-        List<String> events = new ArrayList<>();
-        Elements names = doc.select(".event-list-item__title");
-        Elements costs = doc.select(".event-list-item__type");
-        Elements info = doc.select(".event-list-item__info");
-
-        for (int i = 0, countInfo = 0; i < names.size(); i++) {
-            StringBuilder stringBuilder = new StringBuilder();
-
-            for (int j = countInfo; j < info.size(); j++) {
-                /*
-                If next element in info it's date - break.
-                Next iteration begin in that place.
-                */
-                if (j > countInfo) {
-                    if (isNumeric(info.get(j).text().charAt(0))) {
-                        countInfo = j;
-                        break;
-                    }
-                }
-                stringBuilder.append(info.get(j).text() + " ");
-            }
-            events.add(String.format("%s%s %s ",names.get(i).text(), costs.get(i).text().split("/")[1], stringBuilder.toString()));
-        }
-
-        events.forEach(System.out::println);
+        System.out.println("test");
+//        Document doc = Jsoup.connect("https://it-events.com/hackathons")
+//                .userAgent("Chrome/4.0.249.0 Safari/532.5")
+//                .referrer("http://www.google.com")
+//                .get();
+//
+//        List<String> events = new ArrayList<>();
+//        Elements names = doc.select(".event-list-item__title");
+//        Elements costs = doc.select(".event-list-item__type");
+//        Elements info = doc.select(".event-list-item__info");
+//
+//        for (int i = 0, countInfo = 0; i < names.size(); i++) {
+//            StringBuilder stringBuilder = new StringBuilder();
+//
+//            for (int j = countInfo; j < info.size(); j++) {
+//                /*
+//                If next element in info it's date - break.
+//                Next iteration begin in that place.
+//                */
+//                if (j > countInfo) {
+//                    if (isNumeric(info.get(j).text().charAt(0))) {
+//                        countInfo = j;
+//                        break;
+//                    }
+//                }
+//                stringBuilder.append(info.get(j).text() + " ");
+//            }
+//            events.add(String.format("%s%s %s ",names.get(i).text(), costs.get(i).text().split("/")[1], stringBuilder.toString()));
+//        }
+//
+//        events.forEach(System.out::println);
     }
 
     private static boolean isNumeric(char charAt) {
