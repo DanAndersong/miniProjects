49d127fb152f3c308871003e995a08b4511b0e64
DanAndersong <mr.den.andersong@gmail.com>
2019-09-30 02:27:00 +0300

    add GUI

--- programms/hackathonParser/src/main/java/hackathonParser/App.java
+++ programms/hackathonParser/src/main/java/hackathonParser/App.java
@@ -1,48 +1,49 @@
 package hackathonParser;
 
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import org.jsoup.Jsoup;
 
 public class App
 {
     public static void main( String[] args ) throws IOException {
-//        GUI gui = new GUI();
-        System.out.println("test");
-//        Document doc = Jsoup.connect("https://it-events.com/hackathons")
-//                .userAgent("Chrome/4.0.249.0 Safari/532.5")
-//                .referrer("http://www.google.com")
-//                .get();
-//
-//        List<String> events = new ArrayList<>();
-//        Elements names = doc.select(".event-list-item__title");
-//        Elements costs = doc.select(".event-list-item__type");
-//        Elements info = doc.select(".event-list-item__info");
-//
-//        for (int i = 0, countInfo = 0; i < names.size(); i++) {
-//            StringBuilder stringBuilder = new StringBuilder();
-//
-//            for (int j = countInfo; j < info.size(); j++) {
-//                /*
-//                If next element in info it's date - break.
-//                Next iteration begin in that place.
-//                */
-//                if (j > countInfo) {
-//                    if (isNumeric(info.get(j).text().charAt(0))) {
-//                        countInfo = j;
-//                        break;
-//                    }
-//                }
-//                stringBuilder.append(info.get(j).text() + " ");
-//            }
-//            events.add(String.format("%s%s %s ",names.get(i).text(), costs.get(i).text().split("/")[1], stringBuilder.toString()));
-//        }
-//
-//        events.forEach(System.out::println);
+        Document doc = Jsoup.connect("https://it-events.com/hackathons")
+                .userAgent("Chrome/4.0.249.0 Safari/532.5")
+                .referrer("http://www.google.com")
+                .get();
+
+        ArrayList<String> events = new ArrayList<>();
+        Elements names = doc.select(".event-list-item__title");
+        Elements costs = doc.select(".event-list-item__type");
+        Elements info = doc.select(".event-list-item__info");
+
+        for (int i = 0, countInfo = 0; i < names.size(); i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int j = countInfo; j < info.size(); j++) {
+                /*
+                If next element in info it's date - break.
+                Next iteration begin in that place.
+                */
+                if (j > countInfo) {
+                    if (isNumeric(info.get(j).text().charAt(0))) {
+                        countInfo = j;
+                        break;
+                    }
+                }
+                stringBuilder.append(info.get(j).text() + ", ");
+            }
+            events.add(String.format("%s,%s, %s ",
+                    names.get(i).text(),
+                    costs.get(i).text().split("/")[1],
+                    stringBuilder.toString().trim().substring(0,stringBuilder.toString().length()-2)));
+        }
+
+        GUI gui = new GUI(events);
+        gui.setVisible(true);
     }
 
     private static boolean isNumeric(char charAt) {
--- programms/hackathonParser/src/main/java/hackathonParser/GUI.java
+++ programms/hackathonParser/src/main/java/hackathonParser/GUI.java
@@ -1,16 +1,30 @@
 package hackathonParser;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GUI extends JFrame {
-    JFrame jFrame;
+    private JFrame jFrame;
 
-    public GUI () {
+    public GUI (ArrayList<String> events) {
         super("Window");
         jFrame = new JFrame();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(400, 400);
-        setVisible(true);
+        setSize(800, 600);
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new GridLayout(events.size(),1,1,1));
+        for (String event : events) {
+            jPanel.add(new JButton(event));
+        }
+
+        JScrollPane scrollPane = new JScrollPane(jPanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        getContentPane().add(scrollPane);
+
 
     }
 }
