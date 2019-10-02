d16bc374cfbf2899db7764fb6cff001e3bd54f80
DanAndersong <mr.den.andersong@gmail.com>
2019-10-03 00:43:14 +0300

    add .jar

--- programms/hackathonParser/src/test/java/hackathonParser/AppTest.java
+++ programms/hackathonParser/src/test/java/hackathonParser/AppTest.java
@@ -35,4 +35,14 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 }
