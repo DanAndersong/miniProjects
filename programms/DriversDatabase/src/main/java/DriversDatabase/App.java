8dbe3eb1535cc335e6ca6cbc2fe3542f29035a7a
Den Andeson <mr.den.andrsong@gmail.com>
2020-05-01 15:19:06 +0300

    removed stupid code

--- programms/DriversDatabase/src/main/java/DriversDatabase/App.java
+++
@@ -1,8 +0,0 @@
-package DriversDatabase;
-
-public class App {
-    public static void main(String[] args) {
-        DBController dbController = new DBController();
-        dbController.init();
-    }
-}
--- programms/DriversDatabase/src/main/java/DriversDatabase/model/Categories.java
+++
@@ -1,5 +0,0 @@
-package DriversDatabase.model;
-
-public enum Categories {
-    A, B, C, D, M, M1, M2, G, N, L
-}
